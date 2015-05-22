ec471ba4e5291e6c0b1f5867a393ceed2451b537
timmtonga <timmtonga@gmail.com>
2015-05-22 10:13:16 +0200

    Added version display using git tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def version
+
+    style = "style='background-color:red;'" unless session[:datetime].blank?
+    "ePICS Version: #{APP_VERSION} - <span #{style}>#{(session[:datetime].to_date rescue Date.today).strftime('%A, %d-%b-%Y')}</span>"
+  end
+
+  def welcome_message
+    "Muli bwanji, enter your user information or scan your id card. <span style='font-size:0.6em;float:righti;margin-right: 20px;'>(#{version})</span>".html_safe
+  end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-
+APP_VERSION = `git describe`.gsub("\n", "")
 # Initialize the rails application
 EpicsBdeTool::Application.initialize!
 
